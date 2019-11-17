f71055bc7c00462a82cfc124b33d098ff3a748a3
maythu98 <maythuaung@comquas.com>
2019-11-17 20:48:10 +0630

    initial

---
+++ config/form.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    
+    'user_types' => [
+        [
+            'name' => 'Student',
+            'value' => 1
+        ],
+        [
+            'name' => 'Teacher',
+            'value' => 2,
+        ],
+        [
+            'name' => 'Employeer',
+            'value' => 3,
+        ]
+    ]
+
+]
+
+?>
